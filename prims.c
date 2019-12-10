bf3a39ce38ed39c6c5b2b80cebfeb44a25155b19
sundonsndh <44780622+sundonsndh@users.noreply.github.com>
2019-12-10 17:37:33 +0530

    Update prims.c

--- prims.c
+++ prims.c
@@ -1,46 +1,46 @@
 #include<stdio.h>
-#include<stdlib.h>
-int i,j,k,a,b,u,v,n,ne=1;
-int min,mincost=0,cost[9][9],parent[9];
-int find(int);
-int uni(int,int);
+int a,b,u,v,n,i,j,ne=1;
+int visited[10]={0},min,mincost=0,cost[10][10];
 int main()
 {
-  printf("\n\tImplementation of Kruskal's algorithm\n");
-  printf("\nEnter the no. of vertices:");
+  printf("\nEnter the number of nodes:");
   scanf("%d",&n);
-  printf("\nEnter the cost adjacency matrix:\n");
+  printf("\nEnter the adjacency matrix:\n");
   for(i=1;i<=n;i++)
+  for(j=1;j<=n;j++)
   {
-    for(j=1;j<=n;j++)
-    {
-      scanf("%d",&cost[i][j]);
-      if(cost[i][j]==0)
-        cost[i][j]=999;
-    }
+    scanf("%d",&cost[i][j]);
+    if(cost[i][j]==0)
+      cost[i][j]=999;
+ 
   }
-  printf("The edges of Minimum Cost Spanning Tree are\n");
+  visited[1]=1;
+  printf("\n");
   while(ne < n)
   {
+    
     for(i=1,min=999;i<=n;i++)
+    for(j=1;j<=n;j++)
     {
-      for(j=1;j <= n;j++)
+    if(cost[i][j]< min)
+    {
+      if(visited[i]!=0)
       {
-        if(cost[i][j] < min)
-        {
-          min=cost[i][j];
-          a=u=i;
-          b=v=j;
-        }
+        min=cost[i][j];
+        a=u=i;
+        b=v=j;
       }
+    }  
     }
-    if(u!=v)
+ 
+    if(visited[u]==0 || visited[v]==0)
     {
-      printf("%d edge (%d,%d) =%d\n",ne++,a,b,min);
-      mincost +=min;
+      printf("\n Edge %d:(%d %d) cost:%d",ne++,a,b,min);
+      mincost+=min;
+      visited[b]=1;
     }
+
     cost[a][b]=cost[b][a]=999;
   }
-  printf("\n\tMinimum cost = %d\n",mincost);
-  return 0;
+  printf("\n Minimun cost=%d",mincost);
 }
